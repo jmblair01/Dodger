c6bbf5fc0574d58938f7f027c3af95c4081a1e12
jmblair01 <joanmb02@gmail.com>
2015-05-10 16:19:23 -0400

    Add force

---
+++ Assets/moveit.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var speed: float = 10f;
+var rb: Rigidbody2D;
+
+function Start () {
+ rb = GetComponent.<Rigidbody2D>();
+	 
+}
+
+function Update () {
+
+ 		if (Input.GetKey(KeyCode.LeftArrow))
+         {
+         	
+             //transform.position += Vector3.left * speed * Time.deltaTime;
+            rb.AddForce(Vector2.right * speed);
+         }
+         if (Input.GetKey(KeyCode.RightArrow))
+         {
+        	rb.AddForce(Vector2.right * speed);
+             //transform.position += Vector3.right * speed * Time.deltaTime;
+         }
+         if (Input.GetKey(KeyCode.UpArrow))
+         {
+             	rb.AddForce(Vector2.up * speed);
+         }
+         if (Input.GetKey(KeyCode.DownArrow))
+         {
+             //transform.position += Vector3.down * speed * Time.deltaTime;
+         }
+
+}
